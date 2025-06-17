8f1a7e0d7dce4283f2da77b3ccdef358816126fe
Valtteri Timonen <151870230+vtimonen@users.noreply.github.com>
2025-06-17 18:19:13 +0300

    Add files via upload

---
+++ email-send.js
@@ -0,0 +1,17 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nickname = document.getElementById('nickname').value;
+    const message = document.getElementById('message').value;
+
+    emailjs.send("service_lybqj5r", "template_9f3y2i7", {
+        nickname: nickname,
+        message: message
+    })
+        .then(function () {
+            alert("Thank you for your message!");
+            document.getElementById('contact-form').reset();
+        }, function (error) {
+            alert("There was an error: " + JSON.stringify(error));
+        });
+});
